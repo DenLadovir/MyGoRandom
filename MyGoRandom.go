f9597455cc04a1a1be54c031f7a6c4d1ca76a554
DenLadovir <denis.ladovir@yandex.ru>
2025-06-02 18:48:08 +0300

    fix: make functions more universal

--- MyGoRandom.go
+++ MyGoRandom.go
@@ -7,10 +7,16 @@ import (
 )
 
 func RandFloat(min, max float64) float64 {
+	if max < min {
+		panic("RandInt: max must be >= min")
+	}
 	return min + mathRand.Float64()*(max-min)
 }
 
 func RandInt(min, max int) int {
+	if max < min {
+		panic("RandInt: max must be >= min")
+	}
 	var maxBig *big.Int = big.NewInt(int64(max - min + 1))
 	num, err := cryptoRand.Int(cryptoRand.Reader, maxBig)
 	if err != nil {
@@ -19,12 +25,15 @@ func RandInt(min, max int) int {
 	return int(num.Int64()) + min
 }
 
-func Choice(arr []int) int {
+func Choice[T any](arr []T) T {
+	if len(arr) == 0 {
+		panic("Empty slice")
+	}
 	var index int = RandInt(0, len(arr)-1)
 	return arr[index]
 }
 
-func Shuffle(arr []int) {
+func Shuffle[T any](arr []T) {
 	mathRand.Shuffle(len(arr), func(i, j int) {
 		arr[i], arr[j] = arr[j], arr[i]
 	})
